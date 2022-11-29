1d9e5da981617cea2b444b812106218c948d6fd5
jiyoung-lee <easy9937@gmail.com>
2022-11-29 15:45:47 +0900

    11_29

--- object/table/make_ptable_list.sql
+++ object/table/make_ptable_list.sql
@@ -4,6 +4,6 @@ create table test_part_list
  job_grade int not null)
  partition by list (job_grade)
 (partition p_list_a values in (0),
- partition p_list_a values in (1,2),
- partition p_list_a values in (3,4,5),
- partition p_list_a values in (6,7,8,9));
+ partition p_list_b values in (1,2),
+ partition p_list_c values in (3,4,5),
+ partition p_list_d values in (6,7,8,9));
