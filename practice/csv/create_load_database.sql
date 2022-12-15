43a75fc2f7e667adb9dd7ceb492ff71b200c7b65
Jiseon Lee <102127170+JiseonLee-jul@users.noreply.github.com>
2022-12-15 14:25:33 +0900

    12_15

--- practice/csv/create_load_database.sql
+++ practice/csv/create_load_database.sql
@@ -9,10 +9,10 @@ CREATE TABLE customer (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name varchar(20) NOT NULL,
 	gender char(1) DEFAULT NULL,
-	age int,
-	nickname varchar(20),
+	age int DEFAULT NULL,
+	nickname varchar(20) DEFAULT NULL,
 	phone varchar(20) NOT NULL,
-	email varchar(30)
+	email varchar(30) DEFAULT NULL
 );
 
 CREATE TABLE emp (
