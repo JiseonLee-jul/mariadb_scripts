8c32601c12531d4dcbf4aa777a0368ac32e7abd8
unknown <jiseon7229@naver.com>
2023-03-03 17:56:33 +0900

    2023_03_03

--- practice/12_log.sql
+++ practice/12_log.sql
@@ -1,6 +1,16 @@
------------------- General Log --------------------
+------------------ General Query Log --------------------
 SHOW VARIABLES LIKE '%datadir'; 
 SHOW VARIABLES LIKE '%general%';
 
 SET GLOBAL general_log=1;
 SET GLOBAL general_log_file='general.log';
+
+
+
+------------------ Slow Query Log --------------------
+SHOW VARIABLES LIKE '%slow_query%';
+SHOW VARIABLES LIKE 'long_query_time';
+
+SET GLOBAL slow_query_log=1;
+SET GLOBAL slow_query_log_file='slow.log';
+SET GLOBAL long_query_time=5;
