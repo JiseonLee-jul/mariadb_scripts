fdde47f7278ad113a05f486ea0e84ec2ae6466c1
unknown <jiseon7229@naver.com>
2023-04-07 16:09:32 +0900

    2023_04_07

--- practice/hana.sql
+++ practice/hana.sql
@@ -38,6 +38,71 @@ SELECT
         ELSE 'Grade D' 
     END AS 'Salary Grade'
 FROM employees;
+--- 추가 예제
+SELECT s.Salary_Grade, COUNT(*)
+FROM (SELECT emp_name, 
+CASE WHEN floor(salary/1000000) >= 4 THEN 'Grade A'
+WHEN floor(salary/1000000) = 3 THEN 'Grade B'
+WHEN floor(salary/1000000) = 2 THEN 'Grade C'
+ELSE 'Grade D' 
+END AS 'Salary_Grade'
+FROM employees
+) s
+GROUP BY s.Salary_Grade;
+---
+SELECT s.Salary_Grade, count(*)
+FROM (select emp_name, case when floor(SALARY/1000000) >= 4 then 'Grade A'
+when floor(SALARY/1000000) = 3 then 'Grade B'
+when floor(SALARY/1000000) = 2 then 'Grade C'
+else 'Grade D' end 'Salary_Grade' 
+from employees) s
+GROUP BY s.Salary_Grade
+UNION ALL
+SELECT 'Total', sum(m.count)
+FROM (SELECT s.Salary_Grade, count(*) AS 'count'
+FROM (select emp_name, case when floor(SALARY/1000000) >= 4 then 'Grade A'
+when floor(SALARY/1000000) = 3 then 'Grade B'
+when floor(SALARY/1000000) = 2 then 'Grade C'
+else 'Grade D' end 'Salary_Grade' 
+from employees) s
+GROUP BY s.Salary_Grade) m;
+---
+SELECT c.*, m.* 
+FROM (
+    SELECT s.Salary_Grade, COUNT(*)
+FROM (SELECT emp_name, 
+CASE WHEN floor(salary/1000000) >= 4 THEN 'Grade A'
+WHEN floor(salary/1000000) = 3 THEN 'Grade B'
+WHEN floor(salary/1000000) = 2 THEN 'Grade C'
+ELSE 'Grade D' 
+END AS 'Salary_Grade'
+FROM employees
+) s
+GROUP BY s.Salary_Grade
+) m, copy_dual2 c;
+---
+SELECT 
+    CASE 
+        WHEN c.id = 1 THEN m.Salary_Grade 
+        WHEN c.id = 2 THEN 'Total'
+    END AS 'Salary Grade',
+    CASE 
+        WHEN c.id = 1 THEN m.count
+        WHEN c.id = 2 THEN SUM(m.count)
+    END AS 'COUNT'
+FROM (
+    SELECT s.Salary_Grade, COUNT(*) AS count
+FROM (SELECT emp_name, 
+CASE WHEN floor(salary/1000000) >= 4 THEN 'Grade A'
+WHEN floor(salary/1000000) = 3 THEN 'Grade B'
+WHEN floor(salary/1000000) = 2 THEN 'Grade C'
+ELSE 'Grade D' 
+END AS 'Salary_Grade'
+FROM employees
+) s
+GROUP BY s.Salary_Grade
+) m, copy_dual2 c
+GROUP BY CASE WHEN c.id = 1 THEN m.Salary_Grade ELSE 'Total' END;
 
 
 CREATE TABLE salgrade(grade CHAR(2), lowsalary INT, highsalary INT);
