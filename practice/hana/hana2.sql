fd28862ecc9246352b3585e87eafc0a9a7cbc85e
Jiseon Lee <jiseon7229@naver.com>
2023-04-16 23:35:09 +0900

    2023_04_16

--- practice/hana/hana2.sql
+++ practice/hana/hana2.sql
@@ -1,9 +1,44 @@
--- 숫자를 한글로
-create table lookup
-(valnum int,
-valtext varchar(10)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into lookup
-values(0, ''),(1, '일'),(2, '이'),(3, '삼'),(4, '사'),(5, '오'),(6, '육'),(7, '칠'),(8, '팔'),(9, '구'),(10, '십'),
+---- 조건에 따른 일괄 UPDATE
+-- departments -> departments3 테이블 복제
+CREATE TABLE departments3
+AS SELECT * FROM departments;
+
+-- 4번 업데이트
+UPDATE departments3 SET loc_id = 101 WHERE dept_id = 10;
+UPDATE departments3 SET loc_id = 102 WHERE dept_id = 20;
+UPDATE departments3 SET loc_id = 203 WHERE dept_id = 30;
+UPDATE departments3 SET loc_id = 505 WHERE dept_id = 50;
+
+-- UPDATE 전 확인
+SELECT 
+    dept_id,
+    CASE 
+        WHEN dept_id = 10 THEN 101
+        WHEN dept_id = 20 THEN 102
+        WHEN dept_id = 30 THEN 203
+        ELSE loc_id + 5
+    END AS 'UPDATE 전 확인'
+FROM departments;
+
+-- 1번에 업데이트
+UPDATE departments3 
+SET loc_id = (
+    CASE 
+        WHEN dept_id = 10 THEN 101
+        WHEN dept_id = 20 THEN 102
+        WHEN dept_id = 30 THEN 203
+        ELSE loc_id + 5
+    END
+);
+
+
+---- 숫자를 한글로
+-- lookup 테이블 만들기
+CREATE TABLE lookup
+(valnum INT,
+valtext VARCHAR(10)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO lookup
+VALUES (0, ''),(1, '일'),(2, '이'),(3, '삼'),(4, '사'),(5, '오'),(6, '육'),(7, '칠'),(8, '팔'),(9, '구'),(10, '십'),
 (11, '십일'),(12, '십이'),(13, '십삼'),(14, '십사'),(15, '십오'),(16, '십육'),(17, '십칠'),(18, '십팔'),(19, '십구'),(20, '이십'),
 (21, '이십일'),(22, '이십이'),(23, '이십삼'),(24, '이십사'),(25, '이십오'),(26, '이십육'),(27, '이십칠'),(28, '이십팔'),(29, '이십구'),(30, '삼십'),
 (31, '삼십일'),(32, '삼십이'),(33, '삼십삼'),(34, '삼십사'),(35, '삼십오'),(36, '삼십육'),(37, '삼십칠'),(38, '삼십팔'),(39, '삼십구'),(40, '사십'),
@@ -14,18 +49,27 @@ values(0, ''),(1, '일'),(2, '이'),(3, '삼'),(4, '사'),(5, '오'),(6, '육'),
 (81, '팔십일'),(82, '팔십이'),(83, '팔십삼'),(84, '팔십사'),(85, '팔십오'),(86, '팔십육'),(87, '팔십칠'),(88, '팔십팔'),(89, '팔십구'),(90, '구십'),
 (91, '구십일'),(92, '구십이'),(93, '구십삼'),(94, '구십사'),(95, '구십오'),(96, '구십육'),(97, '구십칠'),(98, '구십팔'),(99, '구십구'),(100, '백'),
 (1000, '천'),(10000, '만');
-
+ 
 SELECT 
     d.dept_id AS '부서코드',
     l.valtext AS '부서코드 한글화'
 FROM departments3 d, lookup l
 WHERE d.dept_id = l.valnum;
 
+-- 백의 자리 숫자 찾기
 SELECT 
     d.loc_id AS '지역코드',
     l.valtext AS '지역코드 한글화'
 FROM departments3 d, lookup l
-WHERE l.valnum < d.loc_id;
+WHERE l.valnum = FLOOR(d.loc_id/100);
+
+SELECT 
+    d.loc_id AS '지역코드',
+    l.valtext AS '지역코드 한글화'
+FROM departments3 d, lookup l
+WHERE l.valnum = FLOOR(d.loc_id/100);
+
+
 
 SELECT 
     dept_id AS '부서코드',
@@ -36,10 +80,10 @@ FROM departments3 d, lookup l;
 
 
 -- 월별실적 증감 출력 (특정 년도 기준)
-SELECT
+SELECT 
 FROM salesamount;
 
--- Grand Total 구하기
+---- Grand Total 구하기
 SELECT
     dept_id AS 'Department',
     SUM(salary) AS 'Sub Total'
@@ -73,13 +117,13 @@ FROM employees e, copy_dual2 c
 GROUP BY CASE WHEN c.id = 1 THEN e.dept_id ELSE 'Total' END;
 
 
--- 1행에 데이터를 1행에 2개 데이터 출력
-create table solarsystem
-(id int,
-name varchar(20));
-insert into solarsystem
-values(1, 'sun'),(2,'mercury'),(3,'venus'),(4,'earth'),(5,'mars'),
-(6,'jupiter'),(7,'saturn'),(8,'uranus'),(9,'neptune'),(10,NULL);
+---- 1행에 데이터를 1행에 2개 데이터 출력
+CREATE TABLE solarsystem
+(id INT,
+name VARCHAR(20));
+INSERT INTO solarsystem
+VALUES(1, 'sun'),(2,'mercury'),(3,'venus'),(4,'earth'),(5,'mars'),
+(6,'jupiter'),(7,'saturn'),(8,'uranus'),(9,'neptune'),(10, NULL);
 
 SELECT
     CASE WHEN MOD(id, 2) = 1 THEN name END AS 'name(first column)',
