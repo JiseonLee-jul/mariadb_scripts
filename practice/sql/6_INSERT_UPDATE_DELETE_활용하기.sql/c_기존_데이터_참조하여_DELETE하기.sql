42f00535b6bd96367820abe018128a7525b70eaa
Jiseon Lee <jiseon7229@naver.com>
2023-04-30 00:16:27 +0900

    2023_04_30

--- practice/sql/6_INSERT_UPDATE_DELETE_활용하기.sql/c_기존_데이터_참조하여_DELETE하기.sql
+++ practice/sql/6_INSERT_UPDATE_DELETE_활용하기.sql/c_기존_데이터_참조하여_DELETE하기.sql
@@ -5,6 +5,10 @@ CREATE TABLE itemdetails_history2
 AS SELECT * FROM itemdetails
    WHERE item_id >= 7;
 
+INSERT INTO itemdetails_history2 
+VALUES (21, 2, NULL),(22, 3, NULL),(23, 4, NULL),(24, 1, NULL),(25, 1, NULL),(26, 3, NULL);
+
+
 SELECT * FROM itemdetails_history1
 EXCEPT
 SELECT * FROM itemdetails_history2;
